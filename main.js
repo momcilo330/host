543a657990683e3cea404b79a47af7c124fb5fd8
momocilo330 <shouldperfect330@gmail.com>
2023-03-16 04:13:53 +0100

    a

--- main.js
+++ main.js
@@ -1764,7 +1764,7 @@ class CustomSequenceForm {
                 }
                 cardDom.classList.add('active')
                 // set selected option in the section
-                document.querySelector('#frm_mcl_').innerHTML = `${cardDom.querySelector('div.rate').innerHTML}`;
+                document.querySelector('#frm_mcl_rate').innerHTML = `${cardDom.querySelector('div.rate').innerHTML}`;
                 document.querySelector('#frm_mcl_selectedoption').innerHTML = `${cardDom.querySelector('div.title').textContent} ${cardDom.querySelector('div.dot-number').textContent} ${cardDom.querySelector('div.distance').textContent}`
             });
         }
